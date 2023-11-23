3047970a7991e387bdedb19cedfecb42a64a5932
babblin <nibla007@hotmail.com>
2023-11-23 15:04:45 +0100

    Add gameplay test

---
+++ tests/word-tests/step_definitions/gameplay.js
@@ -0,0 +1,49 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('that I am on the game page', () => {
+  // TODO: implement step
+});
+
+Given('that I am playing the game', () => {
+  // TODO: implement step
+});
+
+When('I find a word', () => {
+  // TODO: implement step
+});
+
+When('I click Klar button', () => {
+  // TODO: implement step
+});
+
+Then('I get points', () => {
+  // TODO: implement step
+});
+
+When('I click Lås upp & få hjälp button', () => {
+  // TODO: implement step
+});
+
+Then('the red corner is unlocked', () => {
+  // TODO: implement step
+});
+
+Then('I get help', () => {
+  // TODO: implement step
+});
+
+When('I lose', () => {
+  // TODO: implement step
+});
+
+When('I see gameover text', () => {
+  // TODO: implement step
+});
+
+When('I click Spela igen button', () => {
+  // TODO: implement step
+});
+
+Then('the game resets', () => {
+  // TODO: implement step
+});
